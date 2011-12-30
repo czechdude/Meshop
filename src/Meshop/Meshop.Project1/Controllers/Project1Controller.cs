7adc3e1d12bcf2bcf3805254d97e9c3999e7645e
Petr Divis <czechdude@czechdude.com>
2011-12-30 12:52:58 +0100

    prefinal

---
+++ src/Meshop/Meshop.Project1/Controllers/Project1Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Meshop.Framework.Module;
+
+namespace Meshop.Project1.Controllers
+{
+    public class Project1Controller : PluginController
+    {
+        //
+        // GET: /Project1/
+
+
+        [Menu(Name="Project 1")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src/Meshop/Meshop.Project1/Routes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Meshop.Framework.Module;
+
+namespace Meshop.Project1
+{
+    public class Routes : IRoutes
+    {
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute(
+                 "Modules.Project1", // Route name
+                "Project1/{action}", // URL with parameters
+                new { controller = "Project1", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                );
+
+            //routes.MapRoute(
+            //     "Modules.Reviews.Admin", // Route name
+            //    "admin/reviews/{action}", // URL with parameters
+            //    new { controller = "AdminReviews", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            //    );
+        }
+    }
+}
